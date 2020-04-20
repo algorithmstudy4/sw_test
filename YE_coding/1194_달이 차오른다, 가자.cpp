018af73c883b26f8d7e133ebeaf30a7dd46519c7
skyyy6 <35333225+skyyy6@users.noreply.github.com>
2020-04-21 00:23:01 +0900

    YE

---
+++ YE_coding/1194_달이 차오른다, 가자.cpp
@@ -0,0 +1,76 @@
+﻿//백준 https://www.acmicpc.net/problem/1194
+//달이 차오른다, 가자.
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+struct Dir {
+	int x, y;
+};
+
+struct state {
+	int x, y, key; // 열쇠 소지 값, 비트마스크로 저장
+};
+
+Dir dir[4] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
+int n, m, res;
+char arr[50][50];
+bool flag, visit[50][50][1 << 6]; // 방문은 열쇠 소지 값까지 함께 체크
+
+int main() {
+	cin >> n >> m;
+
+	queue<state> q;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+			if (arr[i][j] == '0') {
+				q.push({i, j, 0});
+				visit[i][j][0] = true;
+			}
+		}
+	}
+
+	// BFS
+	while (!q.empty()) {
+		int qsize = q.size();
+
+		while (qsize--) {
+			int x = q.front().x;
+			int y = q.front().y;
+			int k = q.front().key;
+			q.pop();
+
+			// 탈출 발판
+			if (arr[x][y] == '1') {
+				flag = true;
+				break;
+			}
+
+			// 열쇠 발판인 경우 현재 열쇠 소지 값 갱신
+			if (arr[x][y] >= 'a' && arr[x][y] <= 'f') {
+				k = k | 1 << (arr[x][y] - 'a');
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int xx = x + dir[i].x;
+				int yy = y + dir[i].y;
+
+				if (xx < 0 || xx >= n || yy < 0 || yy >= m || visit[xx][yy][k] || arr[xx][yy] == '#') continue; // 좌표 이탈 or 벽인 경우
+				else if ((arr[xx][yy] >= 'A' && arr[xx][yy] <= 'F') && !(k & 1 << (arr[xx][yy] - 'A'))) continue; // 대응하는 열쇠가 없는 경우
+
+				q.push({ xx, yy, k });
+				visit[xx][yy][k] = true;
+			}
+			
+		}
+		if (flag) break;
+		res++;
+	}
+
+	if (!flag) cout << -1;
+	else cout << res;
+
+	return 0;
+}
---
+++ YE_coding/3190_뱀.cpp
@@ -0,0 +1,70 @@
+﻿//백준 https://www.acmicpc.net/problem/3190
+//뱀
+#include<iostream>
+#include<vector>
+#include<deque>
+#include<algorithm>
+
+using namespace std;
+
+struct Dir {
+	int x, y;
+};
+
+Dir dir[] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} }; // 좌표 이동
+
+int N, K, L, sec;
+int map[101][101];
+/*
+	-1 : 사과
+	1 : 뱀
+	0 : 빈 공간
+*/
+
+int main() {
+	deque<pair<int, char>> cmd;
+	deque<pair<int, int>> snake; // 앞뒤에서 push/pop 할 수 있도록 deque 사용
+	snake.push_back({ 1, 1 });
+
+	cin >> N >> K;
+	for (int i = 0; i < K; i++) {
+		int a, b;
+		cin >> a >> b;
+		map[a][b] = -1;  // 사과 좌표
+	}
+
+	cin >> L;
+	for (int i = 0; i < L; i++) {
+		int x;
+		char d;
+		cin >> x >> d;
+		cmd.push_back({ x, d }); // 시간 , 회전 방향
+	}
+
+	int index = 0;
+
+	while (++sec) {
+		int x = snake.back().first + dir[index].x;
+		int y = snake.back().second + dir[index].y;
+
+		if (x <= 0 || x > N || y <= 0 || y > N || map[x][y] == 1) break; // 좌표 이탈 or 몸 충돌
+
+		if (map[x][y] != -1) { // 사과가 없으면 꼬리 빼기
+			map[snake.front().first][snake.front().second] = 0;
+			snake.pop_front();
+		}
+		snake.push_back({ x, y }); // 머리 전진
+		map[x][y] = 1; // 뱀 좌표 표시
+
+		if (!cmd.empty() && cmd.front().first == sec) { // 회전
+			if (cmd.front().second == 'D') index = (index + 1) % 4;
+			else index = (index + 3) % 4;
+
+			cmd.pop_front();
+		}
+	}
+
+	cout << sec;
+
+	return 0;
+}
