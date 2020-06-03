aaa6aa510137c0e5bcd4da46fcf3b1e87e42a61f
sdkfhdquf <47805080+sdkfhdquf@users.noreply.github.com>
2020-06-03 23:25:04 +0900

    Create 7568_덩치.cpp

---
+++ s_coding/7568_덩치.cpp
@@ -0,0 +1,33 @@
+#if 01
+//덩치
+//https://www.acmicpc.net/problem/7568
+//brute force
+#include <iostream>
+using namespace std;
+int main() {
+
+
+	int n;
+	int person[50][2] = { 0 };
+	int rank[50] = { 0 };
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> person[i][0] >> person[i][1];
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == j) { continue; }
+			if ((person[i][0] < person[j][0]) && (person[i][1] < person[j][1]))rank[i]++;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << rank[i]+1 << " ";
+
+	}
+	return 0;
+}
+#endif
