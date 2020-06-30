c66dfd0e9f97cbf11b5dbcc2da3de45a1cf005ee
limseongkyu <96x60812@naver.com>
2020-06-30 16:49:32 +0900

    0701
    2 problems start

---
+++  SK_coding/백준_대학생 기본반/디지털티비.py
@@ -0,0 +1,18 @@
+
+num = int(input())
+channel = list()
+
+for i in range(0, num):
+
+    channel.append(input())
+
+    if channel[i] == 'KBS1':
+
+        KBS1_num = i
+
+    elif channel[i] == 'KBS2':
+
+        KBS2_num = i
+
+
+#  두 채널이 몇번째에 있는지 정보 저장
---
+++  SK_coding/백준_대학생 기본반/카드게임.py
@@ -0,0 +1,12 @@
+
+card_color = [5]
+card_num = [5]
+
+for i in range(0, 5):
+
+    card_color[i], card_num[i] = input().split()
+
+for i in range(0, 5):
+
+    if
+
