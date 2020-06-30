63394fef322083d1214a405f3ffe3f57d046490c
limseongkyu <96x60812@naver.com>
2020-06-30 21:20:51 +0900

    July first
    fifth problem error2

---  SK_coding/백준_대학생 기본반/카드게임.py
+++  SK_coding/백준_대학생 기본반/카드게임.py
@@ -27,19 +27,9 @@ for i in range(0, 5):
             card_color[i] = card_color[j]
             card_color[j] = box
 
-for i in range(0, 5):
+if card_color[0] == card_color[4] and card_num[0] == card_num[1]-1 and card_num[1] == card_num[2]-1 and card_num[2] == card_num[3]-1 and card_num[3] == card_num[4]-1:
 
-    card_color[i] = chr(card_color[i])
-
-if card_color[0] == card_color[4]:
-
-    if card_num[0] == card_num[1]-1 and card_num[1] == card_num[2]-1 and card_num[2] == card_num[3]-1 and card_num[3] == card_num[4]-1:
-
-        score = card_num[4] + 900
-
-    else:
-
-        score = card_num[4] + 600
+    score = card_num[4] + 900
 
 elif (card_num[0] == card_num[3]) or (card_num[1] == card_num[4]):
 
@@ -54,6 +44,10 @@ elif (card_num[0] == card_num[2] and card_num[3] == card_num[4]) or (card_num[0]
     elif card_num[0] == card_num[1] and card_num[2] == card_num[4]:
 
         score = card_num[2]*10 + card_num[0] + 700
+
+elif card_color[0] == card_color[4]:
+
+    score = card_num[4] + 600
 
 elif card_num[0] == card_num[1]-1 and card_num[1] == card_num[2]-1 and card_num[2] == card_num[3]-1 and card_num[3] == card_num[4]-1:
 
