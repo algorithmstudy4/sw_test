f23f786f55b0bc5bf19b7e5fe98cec5c4b88a42a
limseongkyu <96x60812@naver.com>
2020-07-08 21:53:26 +0900

    July second
    error

---  SK_coding/백준_대학생 기본반/올림픽.py
+++  SK_coding/백준_대학생 기본반/올림픽.py
@@ -16,86 +16,74 @@ for i in range(0, country_all):
     silver.insert(num-1, b)
     bronze.insert(num-1, c)
 
-level_list = []
+s1 = set()
+s2 = set()
+s3 = set()
+
+point = gold[country-1]
+level = 1
 
 for i in range(0, country_all):
 
-    point = gold[i]
+    if i == country-1:
 
-    level = 1
+        continue
 
-    for i in range(0, country_all):
+    if gold[i] > point:
 
-        if i == country-1:
+        s1.add(gold[i])
 
-            continue
+    elif gold[i] == point:
 
-        if gold[i] > point:
+        same_gold_country.append(i)
 
-            level = level + 1
+level = level + len(s1)
 
-        elif gold[i] == point:
+if not same_gold_country:
 
-            same_gold_country.append(i)
+    print(level)
 
-    if not same_gold_country:
+else:
 
-        level_list.append(level)
+    point = silver[country-1]
+
+    for i in range(0, len(same_gold_country)):
+
+        if silver[same_gold_country[i]] > point:
+
+            s2.add(silver[same_gold_country[i]])
+
+        elif silver[same_gold_country[i]] == point:
+
+            same_silver_country.append(same_gold_country[i])
+
+    level = level + len(s2)
+
+    if not same_silver_country:
+
+        print(level)
 
     else:
 
-        point = silver[country-1]
+        point = bronze[country-1]
 
-        for i in range(0, len(same_gold_country)):
+        for i in range(0, len(same_silver_country)):
 
-            if silver[same_gold_country[i]] > point:
+            if bronze[same_silver_country[i]] > point:
 
-                level = level + 1
+                s3.add(bronze[same_silver_country[i]])
 
-            elif silver[same_gold_country[i]] == point:
+        level = level + len(s3)
 
-                same_silver_country.append(same_gold_country[i])
+        print(level)
 
-        if not same_silver_country:
-
-            level_list.append(level)
-
-        else:
-
-            point = bronze[country-1]
-
-            for i in range(0, len(same_silver_country)):
-
-                if bronze[same_silver_country[i]] > point:
-
-                    level = level + 1
-
-            level_list.append(level)
-
-new_list = []
-
-for i in level_list:
-
-    if i in new_list:
-
-        if i < level_list[country-1]:
-
-            level_list[country-1] = level_list[country-1] - 1
-
-    elif i not in new_list:
-
-        new_list.append(i)
-
-print(level_list[country-1])
-
-""" 
-4 4 
-1 1 0 0
-2 1 0 0
-3 0 1 0
-4 0 1 0
-
---> error
 """
-
-# time error
+6 6
+1 3 2 0
+2 3 1 0
+3 2 2 0
+4 2 2 0
+5 2 2 1
+6 2 1 0
+error
+"""
