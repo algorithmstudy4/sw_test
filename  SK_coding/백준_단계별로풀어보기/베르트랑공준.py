fc71eac2f2002cec3a909306f397f49f0e1d5389
limseongkyu <96x60812@naver.com>
2020-08-12 22:47:47 +0900

    August second
    1 problems

---  SK_coding/백준_단계별로풀어보기/베르트랑공준.py
+++  SK_coding/백준_단계별로풀어보기/베르트랑공준.py
@@ -1,7 +1,33 @@
-import sys
+from sys import*
+import math
 
 y = 0
 num_list = []
+
+input = stdin.readline
+
+
+def get_primes(n):
+
+    count = 0
+
+    is_primes = [True]*n
+    max_length = math.ceil(math.sqrt(n))
+
+    for i in range(2, max_length):
+
+        if is_primes[i]:
+
+            for j in range(i+i, n, i):
+
+                is_primes[j] = False
+
+    for i in range(2, n):
+        if is_primes[i]:
+            count = count + 1
+
+    return (count)
+
 
 while True:
 
@@ -18,25 +44,6 @@ for i in range(0, len(num_list)-1):
 
     x = 0
 
-    for j in range(num_list[i]+1, (2*num_list[i])+1):
-
-        count = 0
-        k = 2
-
-        while (k*k < j):
-
-            if j % k == 0:
-
-                break
-
-            else:
-
-                count = count + 1
-
-            k = k + 1
-
-        if count == int(j**0.5) - 1:
-
-            x = x+1
+    x = get_primes((2*num_list[i])+1) - get_primes(num_list[i]+1)
 
     print(x)
